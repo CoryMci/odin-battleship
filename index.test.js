2724fcf3bd6b8a4c452c111b3d3e95be4d91b7b3
CoryMci <corymciver@live.ca>
2022-11-07 14:20:33 -0800

    add/test placeShip and receiveAttack functions

--- index.test.js
+++ index.test.js
@@ -1,5 +1,7 @@
-import {ship} from './src/index'
+import {ship, gameBoard} from './src/index'
 
+
+//SHIP TESTS
 test('ship returns correct length', ()=> {
     const battleship = ship(5)
     expect(battleship.length).toBe(5)
@@ -30,4 +32,128 @@ test('When ship(3) is hit 3x, isSunk returns true', ()=> {
     battleship.hit()
     battleship.hit()
     expect(battleship.isSunk).toBe(true)
+})
+
+//GAMEBOARD TESTS
+test('gameboard initializes with empty board', ()=> {
+    const testBoard = gameBoard()
+    expect(testBoard.board[1][1]).toBe(null)
+})
+
+test('gameboard ships initialized correctly', ()=> {
+    const testBoard = gameBoard()
+    expect(testBoard.ships.length).toBe(5)
+})
+
+test('board coordinates greater than 10 return undefined', ()=> {
+    const testBoard = gameBoard()
+    expect(testBoard.board[6][11]).toBe(undefined)
+})
+
+test('gameboard placeShip correctly places ship in direction X', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    testBoard.placeShip(dingy, 3, 6, true)
+    expect(testBoard.board[3][6]).toBe(dingy)
+})
+
+test('gameboard placeShip correctly places ship in direction X', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    testBoard.placeShip(dingy, 3, 6, true)
+    expect(testBoard.board[4][6]).toBe(dingy)
+})
+
+test('gameboard placeShip correctly places ship in direction X', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    testBoard.placeShip(dingy, 3, 6, true)
+    expect(testBoard.board[5][6]).toBe(dingy)
+})
+
+test('gameboard placeShip correctly places ship in direction X', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    testBoard.placeShip(dingy, 3, 6, true)
+    expect(testBoard.board[6][6]).toBe(null)
+})
+
+test('gameboard placeShip correctly places ship in direction Y', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    testBoard.placeShip(dingy, 3, 6, false)
+    expect(testBoard.board[4][6]).toBe(null)
+})
+
+test('gameboard placeShip correctly places ship in direction Y', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    testBoard.placeShip(dingy, 3, 6, false)
+    expect(testBoard.board[3][7]).toBe(dingy)
+})
+
+test('gameboard placeShip correctly returns error string when exceeding board edges', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    const returnvalue = testBoard.placeShip(dingy, 3, 6, false)
+    expect(returnvalue).toBe(undefined)
+})
+
+test('gameboard placeShip correctly returns error string when exceeding board edges', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    const returnvalue = testBoard.placeShip(dingy, 3, 9, false)
+    expect(returnvalue).toBe('invalid coordinates: ship does not fit on board')
+})
+
+test('gameboard placeShip correctly returns error string when colliding with another ship vertically', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    const dingy2 = ship(3)
+    testBoard.placeShip(dingy2, 3, 5, false)
+    const returnvalue = testBoard.placeShip(dingy, 3, 5, false)
+    expect(returnvalue).toBe('invalid coordinates: ship collides with another ship')
+})
+
+test('gameboard placeShip correctly returns error string when colliding with another ship horizontally', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    const dingy2 = ship(3)
+    testBoard.placeShip(dingy2, 2, 1, false)
+    const returnvalue = testBoard.placeShip(dingy, 2, 2, true)
+    expect(returnvalue).toBe('invalid coordinates: ship collides with another ship')
+})
+
+test('recieveAttack correctly updates board', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    const dingy2 = ship(3)
+    testBoard.placeShip(dingy2, 2, 1, false)
+    testBoard.placeShip(dingy, 3, 2, true)
+    testBoard.receiveAttack(3,2)
+
+    expect(testBoard.board[3][2]).toBe('hit')
+})
+
+test('recieveAttack correctly updates board', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    const dingy2 = ship(3)
+    testBoard.placeShip(dingy2, 2, 1, false)
+    testBoard.placeShip(dingy, 3, 2, true)
+    testBoard.receiveAttack(3,2)
+
+    expect(testBoard.board[4][2]).toBe(dingy)
+})
+
+test('recieveAttack correctly updates ships', ()=> {
+    const testBoard = gameBoard()
+    const dingy = ship(3)
+    const dingy2 = ship(3)
+    testBoard.placeShip(dingy2, 2, 1, false)
+    testBoard.placeShip(dingy, 3, 2, true)
+    testBoard.receiveAttack(3,2)
+    testBoard.receiveAttack(4,2)
+
+    expect(dingy.hits).toBe(2)
 })
--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ const ship = function(len){
     hits++
   }
 
-
-
   return {
     get length(){
       return length
@@ -25,4 +23,75 @@ const ship = function(len){
   }
 }
 
-export {ship}
+const gameBoard = function(){
+  const board = {
+                 1:{1:null,2:null,3:null,4:null,5:null,6:null,7:null,8:null,9:null,10:null},
+                 2:{1:null,2:null,3:null,4:null,5:null,6:null,7:null,8:null,9:null,10:null},
+                 3:{1:null,2:null,3:null,4:null,5:null,6:null,7:null,8:null,9:null,10:null},
+                 4:{1:null,2:null,3:null,4:null,5:null,6:null,7:null,8:null,9:null,10:null},
+                 5:{1:null,2:null,3:null,4:null,5:null,6:null,7:null,8:null,9:null,10:null},
+                 6:{1:null,2:null,3:null,4:null,5:null,6:null,7:null,8:null,9:null,10:null},
+                 7:{1:null,2:null,3:null,4:null,5:null,6:null,7:null,8:null,9:null,10:null},
+                 8:{1:null,2:null,3:null,4:null,5:null,6:null,7:null,8:null,9:null,10:null},
+                 9:{1:null,2:null,3:null,4:null,5:null,6:null,7:null,8:null,9:null,10:null},
+                 10:{1:null,2:null,3:null,4:null,5:null,6:null,7:null,8:null,9:null,10:null}
+                }
+  const carrier = ship(5)
+  const battleship = ship(4)
+  const cruiser = ship(3)
+  const submarine = ship(3)
+  const destroyer = ship(2)
+
+  const ships = [carrier, battleship, cruiser, submarine, destroyer]
+
+  const placeShip = function(ship, xCoord, yCoord, horizontal=true){
+    const shipLength = ship.length
+    if (xCoord <= 0 || yCoord <= 0 || xCoord > 10 || yCoord > 10) {
+      return 'invalid Coordinates'
+    }
+    if (!horizontal){
+      if (yCoord + shipLength -1 > 10) {
+        return 'invalid coordinates: ship does not fit on board'
+      }
+      for (let i = 0; i<shipLength; i++) {
+        if (!(board[xCoord][yCoord+i] == null)) {
+          return 'invalid coordinates: ship collides with another ship'
+        }
+      }
+      for (let i = 0; i<shipLength; i++) {
+        board[xCoord][yCoord+i] = ship
+        }
+    } else {
+      if (xCoord + shipLength -1 > 10) {
+        return 'invalid coordinates: ship does not fit on board'
+      }
+      for (let i = 0; i<shipLength; i++) {
+        if (!(board[xCoord+i][yCoord] == null)) {
+          return 'invalid coordinates: ship collides with another ship'
+        }
+      }
+      for (let i = 0; i<shipLength; i++) {
+        board[xCoord+i][yCoord] = ship
+        }
+    }
+  }
+  
+  const receiveAttack = function(xCoord, yCoord) {
+    if (board[xCoord][yCoord] == null) {
+      board[xCoord][yCoord] = 'miss'
+    } else {
+      board[xCoord][yCoord].hit()
+      board[xCoord][yCoord] = 'hit'
+    }
+  }
+
+  return {
+    board,
+    ships,
+    placeShip,
+    receiveAttack
+  }
+}
+
+
+export {ship, gameBoard}
